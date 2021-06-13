3940adca9fab6277daa1675024ba2cab424c7146
NiiL2150 <nail.shamsudinov@gmail.com>
2021-06-13 19:09:06 +0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.SalaryText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // YesButton
+            // 
+            this.YesButton.Location = new System.Drawing.Point(95, 161);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(75, 23);
+            this.YesButton.TabIndex = 0;
+            this.YesButton.Text = "Yes";
+            this.YesButton.UseVisualStyleBackColor = true;
+            this.YesButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.YesButton_MouseClick);
+            // 
+            // NoButton
+            // 
+            this.NoButton.Location = new System.Drawing.Point(336, 161);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(75, 23);
+            this.NoButton.TabIndex = 1;
+            this.NoButton.Text = "No";
+            this.NoButton.UseVisualStyleBackColor = true;
+            this.NoButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NoButton_MouseClick);
+            this.NoButton.MouseHover += new System.EventHandler(this.NoButton_MouseHover);
+            // 
+            // SalaryText
+            // 
+            this.SalaryText.AutoSize = true;
+            this.SalaryText.Location = new System.Drawing.Point(189, 66);
+            this.SalaryText.Name = "SalaryText";
+            this.SalaryText.Size = new System.Drawing.Size(119, 13);
+            this.SalaryText.TabIndex = 2;
+            this.SalaryText.Text = "Do you like your salary?";
+            this.SalaryText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 237);
+            this.Controls.Add(this.SalaryText);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button YesButton;
+        private System.Windows.Forms.Button NoButton;
+        private System.Windows.Forms.Label SalaryText;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NoButton_MouseHover(object sender, EventArgs e)
+        {
+            NoButton.Location = new Point(random.Next(0, 400), random.Next(0, 250));
+        }
+
+        private void NoButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            NoButton.Location = new Point(random.Next(0, 400), random.Next(0, 250));
+        }
+
+        private void YesButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            SalaryText.Text = "How else!";
+        }
+    }
+}
